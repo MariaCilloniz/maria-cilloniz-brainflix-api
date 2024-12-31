081fcf29b8400127ec949bf57b123a57dcd07aec
Maria Cilloniz <majocilloniz@gmail.com>
2024-12-31 15:08:56 -0800

    Backend deployment, fixing coors

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ import cors from 'cors';
 import videosRoutes from './routes/videos.js'
 import "dotenv/config"
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'https://brainflix-application.netlify.app'  
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -21,6 +29,11 @@ app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong!" });
+});
+
 app.listen(PORT, () => {
-    console.log(`Port listening to ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
