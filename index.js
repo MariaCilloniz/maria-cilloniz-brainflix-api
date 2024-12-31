b9bc4b7a315f9a00512be8ecf5e8f4d59ae6ea1c
Maria Cilloniz <majocilloniz@gmail.com>
2024-12-31 14:53:23 -0800

    Fixing bugs with deployment

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ import "dotenv/config"
 
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to BrainFlix API" });
+});
+
 app.use('/videos', videosRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(PORT, () => {
     console.log(`Port listening to ${PORT}`)
